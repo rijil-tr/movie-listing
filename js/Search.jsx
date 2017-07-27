311a9a1bced76ddcaf1d8b42a8f018f1508a9367
Rijil T. R <dev.rijiltr@gmail.com>
2017-07-27 09:01:31 +0530

    Added Search page, Created ShowCards

---
+++ js/Search.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import ShowCard from './ShowCard';
+import preload from '../data.json';
+
+class Search extends Component {
+  constructor(props) {
+    super(props);
+    // this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
+    this.state = {
+      searchTerm: ''
+    };
+  }
+  handleSearchTermChange = event => {
+    this.setState({ searchTerm: event.target.value });
+  };
+  render() {
+    return (
+      <div className="search">
+        <header>
+          <h1>
+            {this.state.searchTerm}
+          </h1>
+          <input
+            onChange={this.handleSearchTermChange}
+            value={this.state.searchTerm}
+            type="text"
+            placeholder="Search"
+          />
+        </header>
+        <div>
+          {preload.shows.map(show =>
+            <ShowCard key={show.imdbID} show={show} />
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Search;
